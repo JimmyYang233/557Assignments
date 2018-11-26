1693a9f0b0c077425177cba2c5e6681d801c79b5
Dan Ning Yang <ydn2012@hotmail.com>
2018-11-26 16:44:17 -0500

    working on q9

--- comp557F18/comp557/a4/Mesh.java
+++ comp557F18/comp557/a4/Mesh.java
@@ -3,6 +3,8 @@ package comp557.a4;
 import java.util.HashMap;
 import java.util.Map;
 
+import comp557.a4.PolygonSoup.Vertex;
+
 public class Mesh extends Intersectable {
 	
 	/** Static map storing all meshes by name */
@@ -23,8 +25,14 @@ public class Mesh extends Intersectable {
 		
 	@Override
 	public void intersect(Ray ray, IntersectResult result) {
-		
+		//System.out.println(soup.faceList.size());
+		//System.out.println(soup.vertexList.size());
 		// TODO: Objective 9: ray triangle intersection for meshes
+		for(int[] face : soup.faceList) {
+			Vertex v1 = soup.vertexList.get(face[0]);
+			Vertex v2 = soup.vertexList.get(face[1]);
+			Vertex v3 = soup.vertexList.get(face[2]);
+		}
 		
 		
 	}
